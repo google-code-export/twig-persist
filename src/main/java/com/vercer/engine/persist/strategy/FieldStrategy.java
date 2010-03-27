9868347e7c87c25a5245c5caedcb466c070f7cb5
John Patterson <john@vercer.com>
2010-03-27 12:47:35 +0700

    Doc

--- src/main/java/com/vercer/engine/persist/strategy/FieldStrategy.java
+++ src/main/java/com/vercer/engine/persist/strategy/FieldStrategy.java
@@ -5,8 +5,26 @@ import java.lang.reflect.Type;
 
 public interface FieldStrategy
 {
+	/**
+	 * @return The property name used for this field
+	 */
 	String name(Field field);
+	
+	
+	/**
+	 * @return The kind name used in the Entity that stores this type
+	 */
 	String typeToKind(Type type);
+	
+	
+	/**
+	 * @return The Type that is represented by this kind name in the Entity
+	 */
 	Type kindToType(String kind);
+	
+	
+	/**
+	 * @return The Type that field values should be converted to
+	 */
 	Type typeOf(Field field);
 }
