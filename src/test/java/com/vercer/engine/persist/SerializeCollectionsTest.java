8900fa69ffae96e5fe7e6099b4c2b2c7a0e28e2b
John Patterson <john@vercer.com>
2010-04-13 12:19:09 +0700

    Fixed bug serializing nulls - thanks Blake Caldwell!

--- src/test/java/com/vercer/engine/persist/SerializeCollectionsTest.java
+++ src/test/java/com/vercer/engine/persist/SerializeCollectionsTest.java
@@ -8,6 +8,8 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import com.google.appengine.api.datastore.Key;
+import com.vercer.engine.persist.TypeWithCollections.TypeWithEnum;
+import com.vercer.engine.persist.TypeWithCollections.TypeWithEnum.MyEnum;
 import com.vercer.engine.persist.annotation.AnnotationObjectDatastore;
 import com.vercer.engine.persist.conversion.CombinedTypeConverter;
 import com.vercer.engine.persist.conversion.DefaultTypeConverter;
@@ -32,16 +34,21 @@ public class SerializeCollectionsTest extends LocalDatastoreTestCase
 		};
 		
 		
-		TypeWithCollection item = new TypeWithCollection();
+		TypeWithCollections item = new TypeWithCollections();
 		item.classes.add(String.class);
 		item.classes.add(Integer.class);
 		item.classes.add(Collection.class);
 		
 		Key key = datastore.store(item);
 		
+		TypeWithEnum thing = new TypeWithEnum();
+		thing.watsit = MyEnum.TWOK10;
+		item.things.add(thing);
+		item.things.add(null);
+		
 		datastore.disassociateAll();
 		
-		TypeWithCollection loaded = datastore.load(key);
+		TypeWithCollections loaded = datastore.load(key);
 		Assert.assertNotNull(loaded);
 	}
 }
